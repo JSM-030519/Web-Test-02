6da9cd7b8322a3982ab14c88b6054b0f22f35fef
Jamie <37302368+JSM-030519@users.noreply.github.com>
2020-02-04 16:11:13 +0000

    ye

--- alert.js
+++ alert.js
@@ -1,3 +1,27 @@
 function AlertBox() {
- 			alert("Button click successful!")
- 		}
+ 	alert("Button click successful!")
+ }
+
+ function ConfirmBox() {
+ 	if (confirm("Press a button")) {
+ 		txt = "You pressed OK!";
+ 	} else {
+ 		txt = "You pressed Cancel!";
+ 	}
+ 	document.getElementById("TextMod1").innerHTML = txt;
+ }
+
+function PromptBox() {
+
+
+ 	var person = prompt("Please enter your name", "Jamie Martin");
+
+	 if (person == null || person == ""){
+ 		txt2 = "User cancelled the prompt, or did not enter anything.";
+	 } else {
+ 		txt2 = "Hello " + person + "! You have successfully activated the prompt box function"
+	 }
+	 document.getElementById("TextMod2").innerHTML = txt2;
+}
+
+
